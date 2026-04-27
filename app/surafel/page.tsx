c1213c364011d6edd09e5ec6a6e2aee358ab50d3
meyatech <surafelmengist2015@gmail.com>
2026-04-27 16:30:45 +0300

    add  the second :

--- app/surafel/page.tsx
+++ app/surafel/page.tsx
@@ -1,6 +1,7 @@
-'use client';
+
 const page = async ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
+  console.log('Slug:', slug);
   return (
     <div className="min-h-screen p-4 flex justify-center items-center gap-2  bg-indigo-700 text-white">
       <h1>Welcome to My Page</h1>
