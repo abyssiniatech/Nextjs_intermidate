6074cf5129101a7a3fbc6ddd2832485ccce578dd
meyatech <surafelmengist2015@gmail.com>
2026-05-01 16:30:11 +0300

    add the day 9:

--- app/intermidate/[slug]/product/[id]/page.tsx
+++ app/intermidate/[slug]/product/[id]/page.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { notFound } from 'next/navigation';
 
-const page = async ({ params }: { params: Promise<{ slug: string; id: string }> }) => {
-  const { slug, id } = await params;
+const Page = ({ params }: { params: { slug: string; id: string } }) => {
+  const { slug, id } = params;
+
+  if (slug !== 'product' || id !== '123') {
+    notFound();
+  }
+
   return (
-    <div className='bg-amber-50 h-screen flex flex-col items-center justify-center text-2xl font-bold  text-gray-800'>
-      <p>productId: <span className='font-semibold text-red-500'>{slug}</span></p>
-      <p>Customer: <span className='font-semibold text-red-500'>{id}</span></p>
+    <div className="bg-amber-50 h-screen flex flex-col items-center justify-center text-2xl font-bold text-gray-800">
+      <p>
+        productId: <span className="font-semibold text-red-500">{slug}</span>
+      </p>
+      <p>
+        Customer: <span className="font-semibold text-red-500">{id}</span>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default page
-
+export default Page;
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="bg-indigo-700 text-3xl text-red-500 flex flex-col items-center justify-center min-h-screen">
+      <h1>Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
