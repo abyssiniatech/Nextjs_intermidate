158406113fa8b3e05630577df9b6ecf0c8b17dce
meyatech <surafelmengist2015@gmail.com>
2026-05-01 16:07:31 +0300

    add the slug:

--- app/intermidate/[slug]/page.tsx
+++ app/intermidate/[slug]/page.tsx
@@ -5,7 +5,7 @@ const page = async ({params}: {params: Promise<{slug: string} >}) => {
   return (
     <div className='bg-amber-50 h-screen flex flex-col items-center justify-center text-2xl font-bold  text-gray-800'>
       <h1>Intermidate</h1>
-      <p>Slug: {slug}</p>
+      <p>Slug: <span className='font-semibold text-red-500'>{slug}</span></p>
     </div>
   )
 }
